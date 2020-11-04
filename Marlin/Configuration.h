c26c002e823de6d2ac07503c394aa637eca8514c
miferkolb <61791039+miferkolb@users.noreply.github.com>
2020-11-04 13:11:40 +0100

    Reduced Z max feedrate

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -745,7 +745,7 @@
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 100, 100, 5, 80 }
+#define DEFAULT_MAX_FEEDRATE          { 100, 100, 2, 80 }
 
 //#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
