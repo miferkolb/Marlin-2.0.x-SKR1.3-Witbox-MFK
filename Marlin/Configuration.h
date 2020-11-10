24493bd8350b37050438e41c97401c145b8e5b2c
miferkolb <61791039+miferkolb@users.noreply.github.com>
2020-11-10 11:16:14 +0100

    SlowHoming 5mms

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1391,7 +1391,7 @@
 
 // Homing speeds (mm/min)
 #define HOMING_FEEDRATE_XY (45*60)
-#define HOMING_FEEDRATE_Z  (6*60)
+#define HOMING_FEEDRATE_Z  (10*60)
 
 // Validate that endstops are triggered on homing moves
 #define VALIDATE_HOMING_ENDSTOPS
